cdb6bdec1a623cdf5ab88097d3e699ec9e78e862
Shawn Doucet <shawndoucet@azuresoft.net>
2019-02-22 06:03:08 -0600

    symbol source tags

---
+++ MigraDoc/src/MigraDoc.Tests/Rendering/ImageRendererTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigraDoc.Tests.Rendering
+{
+    public class ImageRendererTests
+    {
+        //TODO: test changes to XImage CreateXImage(string uri)
+        //one for CreateXImage from fileName
+        //one for CreateXImage for base64
+        //one for Pdf with image from filename
+        //one for Pdf with image from base64
+
+    }
+}
